15f90b2bf5b3816d1e8360ef9a26892d26a771ad
Marco Satta <marcosatta9@gmail.com>
2021-02-20 21:01:28 +0100

    added social component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Social from "./Social"
+
 
 function Home() {
   return (
@@ -12,36 +14,7 @@ function Home() {
             </div>
           </div>
         </div>
-        <div class="rightcolumn">
-          <div class="card">
-            <h3>Seguici</h3>
-            <img
-              src="icons/facebookresizet.png"
-              alt="contatto facebook"
-              title="faccialibro"
-            />
-            <img
-              src="icons/twitterresizet.png"
-              alt="contatto twitter"
-              title="birdo"
-            />
-            <img
-              src="icons/linkedinresizet.png"
-              alt="contatto linkedin"
-              title="inn"
-            />
-            <img
-              src="icons/telegramresizet.png"
-              alt="contatto telegram"
-              title="telegrammo"
-            />
-            <img
-              src="icons/instagramresizet.png"
-              alt="contatto instagram"
-              title="instagrammo"
-            />
-          </div>
-        </div>
+        <Social />
       </div>
     </div>
   );
