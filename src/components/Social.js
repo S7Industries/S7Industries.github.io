802bbd2deadfafd94d39d22f811a00e456edc87a
Marco Satta <marcosatta9@gmail.com>
2021-02-21 10:37:16 +0100

    changed icons reference

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,32 +1,38 @@
 import React from "react";
 
+import facebook  from './icons/facebookresizet.png'
+import twitter  from './icons/twitterresizet.png'
+import linkedin  from './icons/linkedinresizet.png'
+import telegram  from './icons/telegramresizet.png'
+import instagram from './icons/instagramresizet.png' 
+
 function Social() {
   return (
     <div class="rightcolumn">
       <div class="card">
         <h3>Seguici</h3>
         <img
-          src="icons/facebookresizet.png"
+          src = {facebook}
           alt="contatto facebook"
           title="faccialibro"
         />
         <img
-          src="icons/twitterresizet.png"
+          src= {twitter}
           alt="contatto twitter"
           title="birdo"
         />
         <img
-          src="icons/linkedinresizet.png"
+          src= {linkedin}
           alt="contatto linkedin"
           title="inn"
         />
         <img
-          src="icons/telegramresizet.png"
+          src={telegram}
           alt="contatto telegram"
           title="telegrammo"
         />
         <img
-          src="icons/instagramresizet.png"
+          src={instagram}
           alt="contatto instagram"
           title="instagrammo"
         />
